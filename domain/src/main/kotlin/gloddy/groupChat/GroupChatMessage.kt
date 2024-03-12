d6b435dcfceca86159f1d2b0a668a8ea16f84345
jihwan2da <wlghks8982@naver.com>
2024-03-13 02:18:25 +0900

    fix: GroupChatMessage 도메인 모델 변경(MessageType 추가)

--- domain/src/main/kotlin/gloddy/groupChat/GroupChatMessage.kt
+++ domain/src/main/kotlin/gloddy/groupChat/GroupChatMessage.kt
@@ -1,14 +1,17 @@
 package gloddy.groupChat
 
+import gloddy.groupChat.vo.MessageType
 import gloddy.util.UUIDGenerator
 import java.time.LocalDateTime
 
 data class GroupChatMessage(
     val userId: Long,
-    val chatId: Long,
+    val chatId: String,
+    val messageType: MessageType,
+    val content: String?,
     val createdAt: LocalDateTime = LocalDateTime.now(),
     val deleted: Boolean = false,
-    val deletedAt: LocalDateTime,
+    val deletedAt: LocalDateTime? = null,
     val id: String = UUIDGenerator.generate(),
     val sequenceId: Long = 0L
 )
